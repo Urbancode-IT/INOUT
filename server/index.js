b3d3454f769606905c29bd78c017c632f9822c90
Savitha <savitha.saviy@gmail.com>
2025-07-17 14:05:07 +0530

    auth

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
+const authMiddleware = require('./middleware/auth');
 
 const app = express();
 

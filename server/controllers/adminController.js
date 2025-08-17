41fe0dbf713da4ddac96e5bfa94405ac78e43514
Savitha <savitha.saviy@gmail.com>
2025-08-17 23:42:07 +0530

    date of joining

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -38,7 +38,7 @@ const adminController = {
         .populate({
           path: 'user',
           match: { role: 'employee' },
-          select: 'name email role company position department bankDetails'
+          select: 'name email role company position department bankDetails dateOfJoining'
         });
 
       const filteredLogs = logs.filter(log => log.user !== null);
